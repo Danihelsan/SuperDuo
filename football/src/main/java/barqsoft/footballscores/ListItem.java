50e01910180b58bcb306c8b4c8a196ebfe71962b
Danihelsan <danihelsan@gmail.com>
2016-02-27 09:48:16 -0500

    Widget Service and Provider added
    Integrated test RemoteViewsService and AppWidgetProvider
    Code cleaning

---
+++ football/src/main/java/barqsoft/footballscores/ListItem.java
@@ -0,0 +1,6 @@
+package barqsoft.footballscores;
+
+public class ListItem {
+    public String heading,content;
+
+}
---
+++ football/src/main/java/barqsoft/footballscores/WidgetProvider.java
@@ -0,0 +1,49 @@
+package barqsoft.footballscores;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.RemoteViews;
+
+public class WidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        super.onDeleted(context, appWidgetIds);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int i = 0; i < appWidgetIds.length; ++i) {
+            RemoteViews remoteViews = updateWidgetListView(context, appWidgetIds[i]);
+            appWidgetManager.updateAppWidget(appWidgetIds[i], remoteViews);
+        }
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+
+    private RemoteViews updateWidgetListView(Context context, int appWidgetId) {
+        //which layout to show on widget
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_football_layout);
+
+        Intent svcIntent = new Intent(context, WidgetService.class);
+        svcIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+        svcIntent.setData(Uri.parse(svcIntent.toUri(Intent.URI_INTENT_SCHEME)));
+//        remoteViews.setRemoteAdapter(appWidgetId, R.id.listViewWidget, svcIntent);
+        remoteViews.setRemoteAdapter(R.id.listViewWidget, svcIntent);
+        //setting an empty view in case of no data
+        remoteViews.setEmptyView(R.id.empty_view, R.id.listViewWidget);
+        return remoteViews;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+    }
+}
